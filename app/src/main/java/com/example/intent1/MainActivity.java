15e3425efcb469cb78e74102f23cb5fd1631604a
DAM2Alu10 <210176@iessierraguara.es>
2022-11-18 09:40:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/intent1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.intent1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void clic(View view){
+        Intent i = new Intent(this,MainActivity2.class);
+        startActivity(i);
+    }
+}
